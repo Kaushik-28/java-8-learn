1fb71aeeec75c4a3f5c7a1cb717b96cedad07bee
Kaushik Das <Kaushik.Das@Staples.com>
2021-09-15 19:48:27 +0530

    Created from intellij

--- src/main/java/lamdas/RunnableLambdaExample.java
+++ src/main/java/lamdas/RunnableLambdaExample.java
@@ -1,4 +1,19 @@
 package lamdas;
 
 public class RunnableLambdaExample {
+    public static void main(String[] args) {
+       //Java 1.7 code
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Inside old run.");
+            }
+        };
+        new Thread(runnable).start();
+
+        //java 1.8 code
+
+        new Thread(() -> System.out.println("Inside new run.")).start();
+    }
 }
